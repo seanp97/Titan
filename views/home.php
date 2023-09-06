71c2262aaf8eebd2376a001ddb23227c433cc280
Sean Pelser <sean.pelser97@gmail.com>
2023-09-06 20:25:12 +0100

    Update home.php

--- views/home.php
+++ views/home.php
@@ -6,13 +6,6 @@
 
 <h1>Home Page</h1>
 
-<?php 
-
-    Titan::InsertInto("Persons", "FirstName, LastName, EmailAddress", "'Lily', 'Wilson', 'lilywilson@gmail.com'");
-
-
-?>
-
 
 <?php 
     require 'footer.php';
