6549e4d74b58fe4c27e328c032b00d4d7ae5f52d
Sean Pelser <sean.pelser97@gmail.com>
2023-09-08 11:54:30 +0100

    Titan FileUpload function update
    Titan FileUpload function update

--- controllers/upload.php
+++ controllers/upload.php
@@ -3,8 +3,5 @@ require "../titan.php";
 
 if(Titan::IsSubmit()) {
     $uploadStatus = Titan::FileUpload("fileToUpload", array("jpg", "jpeg"), 100);
-
-    if($uploadStatus == "Success") {
-        Titan::Redirect("https://www.youtube.com");
-    }
+    echo $uploadStatus;
 }
