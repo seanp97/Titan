0893637c626143752bda9dd29c7ded0b19036f99
Sean Pelser <sean.pelser97@gmail.com>
2023-09-13 21:16:33 +0100

    main.js update
    main.js update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
 // Titan Javascript
-
-// Initialize Slider
-
-let slider = new Slider(".slider-wrapper", 3000);
-slider.Previous("#prev");
-slider.Next("#next");
-slider.OverLay(75);
-slider.ButtonInlineSlider();
-slider.ButtonArrowsGeneric();
-slider.ObjectFit();
-slider.DotsCenter();
