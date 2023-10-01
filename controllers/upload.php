d298c9e951a48662e890eb3a25a927848b35e4c0
Sean Pelser <sean.pelser97@gmail.com>
2023-10-01 13:31:34 +0100

    Update upload php
    Update upload php

--- controllers/upload.php
+++ controllers/upload.php
@@ -1,10 +1,8 @@
 <?php 
 require "../titan.php";
 
-if(Titan::IsSubmit()) {
-    $uploadStatus = Titan::FileUpload("fileToUpload", array("jpg", "jpeg"), 100);
-    if($uploadStatus != "Unsuccessful") {
-        Titan::InsertInto("Images", "ImageFileName, ImageText", "'$uploadStatus', 'Hello World'");
-        Titan::Redirect("http://localhost/Titan");
-    }
+$uploadStatus = Titan::FileUpload("fileToUpload", array("jpg", "jpeg"), 100);
+if($uploadStatus != "Unsuccessful") {
+    Titan::InsertInto("Images", "ImageFileName, ImageText", "'$uploadStatus', 'Hello World'");
+    Titan::Redirect("http://localhost/Titan");
 }
