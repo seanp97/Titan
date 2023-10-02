cb43460d21a496f84d21a34996d0fc5130638d9d
Sean Pelser <sean.pelser97@gmail.com>
2023-10-02 18:53:06 +0100

    Upload file working

--- controllers/upload.php
+++ controllers/upload.php
@@ -3,6 +3,5 @@ require "../titan.php";
 
 $uploadStatus = Titan::FileUpload("fileToUpload", array("jpg", "jpeg"), 100);
 if($uploadStatus != "Unsuccessful") {
-    Titan::InsertInto("Images", "ImageFileName, ImageText", "'$uploadStatus', 'Hello World'");
     Titan::Redirect("http://localhost/Titan");
 }
