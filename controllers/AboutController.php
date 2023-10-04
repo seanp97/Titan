0e105732b11b70e451beeec6be25cdb54da4a146
Sean Pelser <sean.pelser97@gmail.com>
2023-10-04 14:23:27 +0100

    comments update

--- controllers/AboutController.php
+++ controllers/AboutController.php
@@ -6,7 +6,7 @@ require_once './core/view-loader.php';
 class About {
 
     static function index() {
-        // Load about.php file in views/About folder
+        // Load index.php file in views/About folder
         view("About/index");
     }
 
--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -6,7 +6,7 @@ require_once './core/view-loader.php';
 class Contact {
 
     static function index() {
-        // Load contact.php file in views/Contact folder
+        // Load index.php file in views/Contact folder
         view("Contact/index");
     }
 
